93cc8560691c520c948b530e10ddb562e90e179c
Ballista Milsim <ballista.milsim@gmail.com>
2017-04-17 16:39:54 +0200

    tb_rhs_fools_road.zargabad mission added

---
+++ factions/BLU_F_roleRatio.hpp
@@ -0,0 +1,20 @@
+// JAF - Army (Leon Special)
+// Author: Cole
+
+{
+	// {id, "caption", teamRatio, minimum, minTeamSize, minSquadSize, squadRatio}
+	{__ICE_gear_Rifleman, "Rifleman", __unlimited, 0, 0, 0, 0},
+	{__ICE_gear_Grenadier, "Grenadier", 6, 2, 0, 2, 0},
+	{__ICE_gear_MG, "MG", 5, 2, 0, 3, 0},
+	{__ICE_gear_Medic, "Medic", 4, 2, 0, 3, 0},
+	{__ICE_gear_CombatEngineer, "Combat Engineer", 6, 3, 0, 3, 0},
+	{__ICE_gear_LightAT, "Light AT", 4, 2, 0, 3, 0},
+	{__ICE_gear_HeavyAT, "Heavy AT", 8, 2, 0, 3, 0},
+	{__ICE_gear_AA, "Anti-Air", 7, 1, 0, 4, 0},
+	{__ICE_gear_Marksman, "Marksman", 8, 2, 0, 3, 0},
+	{__ICE_gear_Sniper, "Sniper", 10, 0, 0, 2, 0},
+	{__ICE_gear_SL, "Squad Leader", __unlimited, 0, 0, 1, 0},
+	{__ICE_gear_Crewman, "Crewman", __unlimited, 0, 0, 0, 0},
+	{__ICE_gear_Pilot, "Pilot", __unlimited, 0, 0, 0, 0},
+	{__ICE_gear_Diver, "UAV Operator", 30, 0, 0, 3, 0}
+},
---
+++ factions/OPF_F_roleRatio.hpp
@@ -0,0 +1,20 @@
+// RUS - Marines (EMR-Summer)
+// Author: Cole
+
+{
+	// {id, "caption", teamRatio, minimum, minTeamSize, minSquadSize, squadRatio}
+	{__ICE_gear_Rifleman, "Rifleman", __unlimited, 0, 0, 0, 0},
+	{__ICE_gear_Grenadier, "Grenadier", 6, 2, 0, 3, 0},
+	{__ICE_gear_MG, "MG", 4, 3, 0, 3, 0},
+	{__ICE_gear_Medic, "Medic", 4, 2, 0, 3, 0},
+	{__ICE_gear_CombatEngineer, "Combat Engineer", 5, 2, 0, 3, 0},
+	{__ICE_gear_LightAT, "Light AT", 4, 2, 0, 3, 0},
+	{__ICE_gear_HeavyAT, "Medium AT", 7, 2, 0, 3, 0},
+	{__ICE_gear_AA, "Anti-Air", 7, 1, 0, 3, 0},
+	{__ICE_gear_Marksman, "Marksman", 7, 2, 0, 3, 0},
+	{__ICE_gear_Sniper, "Sniper", 10, 1, 0, 2, 0},
+	{__ICE_gear_SL, "Squad Leader", __unlimited, 0, 0, 1, 0},
+	{__ICE_gear_Crewman, "Crewman", __unlimited, 0, 0, 0, 0},
+	{__ICE_gear_Pilot, "Pilot", __unlimited, 0, 0, 0, 0},
+	{__ICE_gear_Diver, "Diver", __unlimited, 0, 0, 2, 0}
+},
